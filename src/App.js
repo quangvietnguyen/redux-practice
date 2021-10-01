08313f583e75ce1344c789c1130a0a08099c3475
nguye374 <nguye374@sheridancollege.ca>
2021-09-30 22:57:00 -0400

    Sep 30 third commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
+import { Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const notification = useSelector(state => state.ui.notification);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      }))
+      const response = await fetch('https://food-order-app-3b13d-default-rtdb.firebaseio.com/cart',{
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      })
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed!')
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!',
+      }))
+
+      //const responseData = await response.json();
+    };
+
+    if (isInitial){
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!',
+      }))
+    })
+  }, [cart,dispatch])
   return (
-    <Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
+    </Fragment>
   );
 }
 
